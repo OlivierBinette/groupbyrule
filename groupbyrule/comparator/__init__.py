4f1421179e0a08a1f3e740b3202769f940304766
Olivier Binette <olivier.binette@gmail.com>
2022-01-08 19:16:13 -0500

    Add warning on shared library load failure

--- groupbyrule/comparator/__init__.py
+++ groupbyrule/comparator/__init__.py
@@ -2,4 +2,8 @@ try:
     from groupbyrule.comparator._comparator import Comparator, StringComparator
     from groupbyrule.comparator._levenshtein import Levenshtein
 except:
-    from .levenhstein import Comparator, Levenshtein
+    import warnings
+    warnings.warn(
+        "Could not load C++ shared library. Using pure Python implementation instead.")
+    from .comparator import Comparator, StringComparator
+    from .levenhstein import Levenshtein
